0c120feb421a2d445de43d3c663432cf5bcc4359
bamamoudou <mamoudouba8787@gmail.com>
2021-03-03 18:25:56 +0100

    Adding Javadocs and Swagger

--- src/main/java/com/tripmaster/trippricer/controller/PriceController.java
+++ src/main/java/com/tripmaster/trippricer/controller/PriceController.java
@@ -12,12 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tripmaster.trippricer.model.ProviderListWrapper;
 import com.tripmaster.trippricer.service.PriceService;
 
+import io.swagger.annotations.Api;
+
 @RestController
+@Api(description="Price microservice for TourGuide")
 public class PriceController {
 	@Autowired
 	private PriceService pricerService;
 	private final Logger logger = LoggerFactory.getLogger(PriceController.class);
 
+	/**
+	 * Gets a list of a user's available trip deals
+	 * 
+	 * @param tripPricerApiKey
+	 * @param userId
+	 * @param numberOfAdults
+	 * @param numberOfChildren
+	 * @param tripDuration
+	 * @param cumulativeRewardPoints
+	 * @return list of trip deals
+	 */
 	@RequestMapping("/trip-deals")
 	public ProviderListWrapper getTripDeals(@RequestParam String tripPricerApiKey, @RequestParam UUID userId,
 			@RequestParam int numberOfAdults, @RequestParam int numberOfChildren, @RequestParam int tripDuration,
